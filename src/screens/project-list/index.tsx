f1e27486cd53f70c12724ed44d031c34dcefa55e
zhaosai <1162838487.com>
2022-12-30 20:28:16 +0800

    feat:使用ts进行类型检查

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,33 +3,35 @@ import {SearchPanel} from "./search-panel";
 import {List} from "./list";
 import {useEffect, useState} from "react";
 import * as qs from 'qs'
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebaunce, useMount} from "../../utils";
 const  apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = ()=>{
+
   const [users,setUsers] = useState([])
   const [param,setParam] = useState({
     name:'',
     personId:''
   })
   const [list,setList] = useState([])
+  const debauncedParam = useDebaunce(param,2000)
   useEffect(()=>{
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debauncedParam))}`).then(
       async response => {
         if (response.ok){
           setList(await response.json())
         }
       })
-  },[param])
-  useEffect(()=>{
+  },[debauncedParam])
+  useMount(()=>{
     fetch(`${apiUrl}/users`).then(
       async response => {
         if (response.ok){
           setUsers(await response.json())
         }
       })
-  },[])
+  })
   return <div>
     <SearchPanel param={param} setParam={setParam} users={users} />
-    <List list={list} setList={setList} users={users} />
+    <List list={list}users={users} />
   </div>
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,47 @@
+import {useEffect, useState} from "react"
+
+const isFalsy = (value:any) => (value === 0 ? false : !value);
+//在一个函数不能修改原对象
+export const cleanObject = (object:object) => {
+  const result = JSON.parse(JSON.stringify(object));
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    //当value为0时，会被误删除
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  })
+  return result
+}
+//进行初始化页面useEffect依赖为空数组的自定义hook
+export const useMount = (callback:() => void)=>{
+  useEffect(()=>{
+    callback()
+  },[])
+}
+
+//原始防抖原生函数
+const debaunce = (func:() => void,delay?:number) => {
+  let timeout:any
+  return () => {
+    if(timeout){
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(()=>{
+      func()
+    },delay)
+  }
+}
+
+//custom hook是因为我们需要使用其他的hook
+export const useDebaunce = (value:any,delay?:number ) => {
+  //使用useState是因为我们需要react检测到value的变化
+  const [debauncedValue,setDebauncedValue] = useState(value)
+  //每次在value变化以后设置一个定时器
+  useEffect(()=>{
+    const timeout = setTimeout(()=> setDebauncedValue(value),delay)
+    //上次useEffect执行之后运行return函数
+    return ()=>clearTimeout(timeout)
+  },[value,delay])
+  return debauncedValue
+}
