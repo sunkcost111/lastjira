8ac9a43c0c3f9ea3406ac8e1940f8e5b791ac141
zhaosai <1162838487.com>
2022-12-30 22:41:42 +0800

    feat:使用auth-provider实现JWT的token

---
+++ src/auth-provider.ts
@@ -0,0 +1,45 @@
+//操控JWT的token 在真实环境中,只用firebase第三方服务，则不需要开发者开发
+
+import {User} from "./screens/project-list/search-panel";
+
+const localStorageKey = '__auth_provider_token__'
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const getToken = () => window.localStorage.getItem(localStorageKey)
+
+export const handleUserResponse = ({user}:{user:User}) => {
+  window.localStorage.setItem(localStorageKey,user.token || '')
+  return user
+}
+
+export const login = (data:{username:string,password:string}) => {
+  fetch(`${apiUrl}/login`,{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(
+    async response => {
+      if (response.ok){
+        handleUserResponse(await response.json())
+      }
+    })
+}
+
+export const register = (data:{username:string,password:string}) => {
+  fetch(`${apiUrl}/register`,{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(
+    async response => {
+      if (response.ok){
+        handleUserResponse(await response.json())
+      }
+    })
+}
+
+export const logout = () => window.localStorage.removeItem(localStorageKey)
