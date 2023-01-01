a3578e0168a5ba98153236f7d40938ad0cd544b6
zhaosai <1162838487.com>
2023-01-01 11:35:12 +0800

    feat:antd和emotion

--- src/unauthenicated-app/register.tsx
+++ src/unauthenicated-app/register.tsx
@@ -1,6 +1,7 @@
 import React, {FormEvent} from "react"
 import {useAuth} from "context/auth-context"
 import {Form,Input,Button} from 'antd'
+import {LongButton} from "./index";
 
 const apiUrl = process.env.REACT_APP_API_URL
 export const RegisterScreen = () => {
@@ -17,7 +18,7 @@ export const RegisterScreen = () => {
       <Input placeholder={'密码'} type={"password"} id={"password"} />
     </Form.Item>
     <Form.Item>
-      <Button htmlType={'submit'} type={'primary'}>注册</Button>
+      <LongButton htmlType={'submit'} type={'primary'}>注册</LongButton>
     </Form.Item>
   </Form>
 }
